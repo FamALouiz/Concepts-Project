1a34e5e31f3acd884e0d2017d5b0645bcac513cf
Fam Awad <fam@awadlouis.com>
2024-04-14 21:05:46 +0200

    Adding group_days predicate

--- Team_X.pl
+++ Team_X.pl
@@ -2,3 +2,8 @@
 % instead of needing to consult the files individually.
 % This line MUST be included in the final submission.
 :- ['transport_kb', 'slots_kb'].
+:- use_module(library(lists)).
+
+group_days(Group, L):-
+    findall(day_timing(Week, Day), scheduled_slot(Week, Day, _, _, Group), L1),
+    list_to_set(L1, L).
