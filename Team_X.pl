2e177a5738d3548702fcae2ddf2320be6c7e56ba
Fam Awad <fam@awadlouis.com>
2024-04-14 21:07:00 +0200

    Updating spacing and structure

--- Team_X.pl
+++ Team_X.pl
@@ -2,17 +2,13 @@
 % instead of needing to consult the files individually.
 % This line MUST be included in the final submission.
 :- ['transport_kb', 'slots_kb'].
+:- use_module(library(lists)).
 
 /*
 gets a list of all slots of a specific group in a certain week and day
 */
 day_slots(Group, Week, Day, Slots):-
 	findall(X, scheduled_slot(Week, Day, X, _, Group), Slots).
-
-	
-	
-	
-:- use_module(library(lists)).
 
 group_days(Group, L):-
     findall(day_timing(Week, Day), scheduled_slot(Week, Day, _, _, Group), L1),
