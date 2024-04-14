c2a37851e464216b290ee26bf2462cdf979cad46
Daniel Michael <danielmichaelghaly@gmail.com>
2024-04-14 20:52:53 +0200

    I made the 4.1.2 day_slots/4 predicate

--- Team_X.pl
+++ Team_X.pl
@@ -2,3 +2,13 @@
 % instead of needing to consult the files individually.
 % This line MUST be included in the final submission.
 :- ['transport_kb', 'slots_kb'].
+
+/*
+gets a list of all slots of a specific group in a certain week and day
+*/
+day_slots(Group, Week, Day, Slots):-
+	findall(X, scheduled_slot(Week, Day, X, _, Group), Slots).
+
+	
+	
+	
