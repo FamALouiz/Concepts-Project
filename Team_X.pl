8f333351b89d98fc83e98d3914ffb30589dbe761
HamzaAlmosalamy <hamzaabdelghanyy@gmail.com>
2024-05-02 01:35:01 +0200

    Changes in Travel_plan (not really)
    There are 2 blocks of codes, one that's working and the other one is not working
    
    line 150-183, is the code that's working, except for the shortest predicate, it's commented already on it
    
    line 195-234, is the code that wwas last pushed, it's not working somehow, i didn't look into it though

--- Team_X.pl
+++ Team_X.pl
@@ -145,8 +145,55 @@ connected(Source, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Pr
     New_Duration =< Max_Duration,
     append_connection(Source, Intermediate, Duration_Added, Transportation, Temp_Routes, Routes_New),
     connected_temp(Intermediate, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Routes, New_Duration, Routes_New).*/
+
+
+%   This is the code that's working, used the one that was right before my push 
+%   (the comment on that change was "made some changes in travel plan procedure" by daniel)
+    travel_plan([],_,_,_,[]).
+travel_plan([H|T], Group, Max_Duration, Max_Routes, Journeys):-
+    earliest_slot(Group, Week, Day, Slot_Num),
+    scheduled_slot(Week, Day, Slot_Num,_,Group),
+    slot(Slot_Num,Start_Hour, Start_Min),
+    connected(H, borsigwerke, Week, Day, Max_Duration, Max_Routes, Duration, Routes),
     
-travel_plan([],_,_,_,[]).
+    append([journey(Week,Day, Start_Hour, Start_Min, Duration, Routes)],Till_now_Journeys,Journeys),
+    travel_plan(T,Group, Max_Duration, Max_Routes, Till_now_Journeys).
+    %Journeys = [H|T],
+    %shortest(T, H, ShortestJourney).
+
+    travel_plan([H|T], Group, Max_Duration, Max_Routes, Journeys):-
+        earliest_slot(Group, Week, Day, Slot_Num),
+    scheduled_slot(Week, Day, Slot_Num,_,Group),
+    slot(Slot_Num,Start_Hour, Start_Min),
+    connected(H,tegel, Week, Day, Max_Duration, Max_Routes, Duration, Routes),
+    append([journey(Week,Day, Start_Hour, Start_Min, Duration, Routes)],Till_now_Journeys,Journeys),
+    travel_plan(T,Group, Max_Duration, Max_Routes, Till_now_Journeys).
+    %Journeys=[H|T],
+    %shortest(T, H, ShortestJourney).
+
+
+% idk about the shortest yet
+    /*shortest([], Acc, Acc).
+shortest([H|T], Acc, ShortestJourney):-
+    H = journey(_,_, _, _, Duration1, _),
+    Acc = journey(_,_, _, _, Duration2, _),
+    Duration2>Duration1,
+    Acc = H,
+    shortest(T, Acc, ShortestJourney).*/
+
+
+
+
+
+
+
+
+
+
+
+
+  % This is the codee that was last pushed
+/*travel_plan([],_,_,_,[]).
 travel_plan([H|T], Group, Max_Duration, Max_Routes, Journeys):-
     group_days(Group, Day_Timings),
     travel_planHelper([H|T], Group, Max_Duration, Max_Routes, Day_Timings, Journeys).
@@ -184,4 +231,4 @@ shortest([H|T], Acc, shortestJourney):-
     Acc = journey(_,_, _, _, Duration2, _),
     Duration2>Duration1,
     Acc = H,
-    shortest(T, Acc, shortestJourney).
+    shortest(T, Acc, shortestJourney). */
