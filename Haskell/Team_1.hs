bcb6e6b1ebbe0b1fc0efa9ee491ea1ee6bdd0d17
Fam Awad <fam@awadlouis.com>
2024-05-22 11:27:49 +0200

    Added dig implementation + removeItem helper function to remove an item from a list

--- Haskell/Team_1.hs
+++ Haskell/Team_1.hs
@@ -7,3 +7,15 @@ data MyState = Null | S Cell [Cell] String MyState deriving (Show, Eq)
 -- Up
 up :: MyState -> MyState
 up (S (x, y) listOfCells s state) = if x - 1 < 0 then Null else S (x - 1, y) listOfCells "up" (S (x, y) listOfCells s state)
+
+
+-- Remove Item helper function
+removeItem _ [] = []
+removeItem x (y:ys) | x == y    = removeItem x ys
+                    | otherwise = y : removeItem x ys
+
+-- Dig
+dig :: MyState -> MyState
+dig (S cell listOfCells s state) = if any (==cell) listOfCells 
+    then S cell (removeItem cell listOfCells) "dig" (S cell listOfCells s state) 
+    else Null
