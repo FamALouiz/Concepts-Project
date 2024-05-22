a0d9a61c36224765312e82bd5ceea67bce99acef
Fam Awad <fam@awadlouis.com>
2024-05-22 11:20:31 +0200

    Added up type

--- Haskell/Team_1.hs
+++ Haskell/Team_1.hs
@@ -3,5 +3,7 @@ type Cell = (Int,Int)
 
 -- MyState
 data MyState = Null | S Cell [Cell] String MyState deriving (Show, Eq)
- 
+
+-- Up
+up :: MyState -> MyState
 up (S (x, y) listOfCells s state) = if x - 1 < 0 then Null else S (x - 1, y) listOfCells "up" (S (x, y) listOfCells s state)
